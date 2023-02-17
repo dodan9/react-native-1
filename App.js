6dae7e83552f623c87539e69008a13c3cc5baa4c
DODAN9 <9139a90@naver.com>
2023-02-17 17:53:36 +0900

    layout
    - react native으ㅣ view 는 flex를 기본값으로 가진다
    - flex direction의 기본값은 row가 아닌 column이다.
    - 최상위 view에 flex값을 정해주지 않으면 자식 view에 flex를 주어도 의미 업다

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Sta } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>My first app.</Text>
-      <StatusBar style='auto' />
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      {/* <Text style={styles.text1}>My first app</Text>
+      <StatusBar style='auto' /> */}
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 8, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
@@ -16,5 +19,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text1: {
+    fontSize: 40,
+    color: "green",
   },
 });
